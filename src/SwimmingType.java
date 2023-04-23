c1ae28cf79128b9d574813e5b50eba1a19158316
anasm20 <agrimohamad@gmail.com>
2023-04-23 22:56:12 +0200

    v4

--- src/SwimmingType.java
+++ src/SwimmingType.java
@@ -1,6 +1,10 @@
 /**
- * List of SwimmingType
+ * Dieser Code definiert einen Aufzählungstyp (enum) mit dem Namen
+ * "SwimmingType",
+ * der zwei Konstanten enthält: "BACKSTROKE" und "BUTTERFLY".
+ * Diese Konstanten repräsentieren verschiedene Schwimmstile.
  */
+
 public enum SwimmingType {
     BACKSTROKE,
     BUTTERFLY
